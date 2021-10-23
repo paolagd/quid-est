ffccc8fe81326fbb12fe5c20058e326469d191d1
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-23 18:14:04 -0400

    layout & style Login componennt & add it to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,41 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import "./Login.css";
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
   return (
-    <div>
-
+    <div className="login">
+      <div className="login__container">
+        <input
+          type="text"
+          className="login__textBox"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail Address"
+        />
+        <input
+          type="password"
+          className="login__textBox"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button
+          className="login__btn"
+        >
+          Login
+        </button>
+        <button className="login__btn login__google">
+          Login with Google
+        </button>
+        <div>
+          Don't have an account? <Link to="/register">Register</Link> now.
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
-export default Login
+export default Login;
