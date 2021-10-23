fc1bbb6726d617cb49649bb6e82682cef9dc9a03
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-23 18:21:24 -0400

    layout & style Register component & add it to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from './Login';
+import Register from './Register';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={Register} />
         </Switch>
       </Router>
     </div>
--- src/Register.js
+++ src/Register.js
@@ -1,11 +1,48 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import "./Register.css";
 function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const history = useHistory();
   return (
-    <div>
-
+    <div className="register">
+      <div className="register__container">
+        <input
+          type="text"
+          className="register__textBox"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Full Name"
+        />
+        <input
+          type="text"
+          className="register__textBox"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail Address"
+        />
+        <input
+          type="password"
+          className="register__textBox"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button className="register__btn">
+          Register
+        </button>
+        <button
+          className="register__btn register__google"
+        >
+          Register with Google
+        </button>
+        <div>
+          Already have an account? <Link to="/">Login</Link> now.
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
-export default Register
+export default Register;
