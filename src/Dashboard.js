4aba39f90c913da43de178376fde7c4294cd169a
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-23 22:41:05 -0400

    if user is not logged in, divert to /login path

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,16 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useAuthState } from 'react-firebase-hooks/auth'
 import "./Dashboard.css";
-import { logout } from "./firebase";
+import { auth, logout } from "./firebase";
 function Dashboard() {
   const logoutUser = () => {
     logout();
   }
   const [name, setName] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (loading) return;
+    if (!user) history.replace('/login');
+  }, [user, loading])
   return (
     <div className="dashboard">
       <div className="dashboard__container">
         Logged in as
-        <div>{name}</div>
+        <div>{user?.email}</div>
         <button className="dashboard__btn" onClick={logoutUser}>
           Logout
         </button>
