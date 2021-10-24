dc7fbdc12bd43c7a2d9142642207477d61dcc774
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-23 23:10:06 -0400

    add firebase api identifiers to .env

--- src/firebase.js
+++ src/firebase.js
@@ -14,12 +14,12 @@ import {
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCEBshVIipxOycgm2NC14UPgXtspvBhrn0",
-  authDomain: "another-react-firebase.firebaseapp.com",
-  projectId: "another-react-firebase",
-  storageBucket: "another-react-firebase.appspot.com",
-  messagingSenderId: "376944110891",
-  appId: "1:376944110891:web:f4d46e6307579ebc4aa96e"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
