2195f9318777c50d5dfa2ed1d1bba5572fc6a8b1
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-27 14:29:34 -0400

    add links to relevant component from sidebar

--- src/components/SideBar/SideBarItem.js
+++ src/components/SideBar/SideBarItem.js
@@ -1,10 +1,10 @@
 export default function SideBarItem(props) {
-  const { title, icon } = props;
+  const { title, icon, path } = props;
   const i = `fas ${icon}`;
 
   return (
     <li className="nav-item">
-      <a className="nav-link" href="index.html">
+      <a className="nav-link" href={path}>
         <i className={i}></i>
         <span>{title}</span>
       </a>
