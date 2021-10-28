5b6cd268835e4a393a41f30c2b3dbe000a9bcd17
Paola Garcia <pacri14@gmail.com>
2021-10-28 10:39:43 -0400

    Adds validation for dbs with less than 10 things to shuffle/slice

--- src/helpers/quiz.js
+++ src/helpers/quiz.js
@@ -1,4 +1,10 @@
-export function shuffle(things) { 
+export function shuffle(things) {
   const shuffled = [...things];
-  return shuffled.sort(() => Math.random() - 0.5).slice(0, 10); 
+  let n = 10;
+  //Makes sure the array has enough results to slice
+  if (shuffled.length < 10) {
+    n = shuffle.length;
+  }
+
+  return shuffled.sort(() => Math.random() - 0.5).slice(0, n);
 }
