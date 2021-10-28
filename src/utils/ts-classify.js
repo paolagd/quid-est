9b987caa564821bcce9f5a3f75cbf7a62779b6d4
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-27 20:15:52 -0400

    get a burst of predictions until a proper prediction is obtained

--- src/utils/ts-classify.js
+++ src/utils/ts-classify.js
@@ -1,23 +1,29 @@
 import * as mobilenet from "@tensorflow-models/mobilenet";
 import '@tensorflow/tfjs-backend-webgl';
 
-export async function loadModel(callback) {
-  const loadedModel = await mobilenet.load();
-  callback(loadedModel);
-}
-
-/* Returns an array of three prediction objects {name:__, probability:__} from image
- * param1: model: the loaded tensorflow mobilenet model
- * param2: callback: callback function to execute when predictions are returned
- * param3: reference to image from DOM
- */
-export async function classify(model, callback, imgRef) {
-  if (model === null) return;
-  const predictions = await model.classify(imgRef.current);
-  callback(predictions);
-  console.log(predictions);
-}
-
 export function parsePercent(num) {
-  return (num*100).toFixed(2);
+  return (num * 100).toFixed(2);
+}
+
+const pred = async (model, img) => {
+  // Classify the image.
+  const predictions = await model.classify(img);
+  console.log(`burst of predictions`, predictions);
+  return predictions;
+}
+
+const mlBurst = async (img) => {
+  // Load the model.
+  const model = await mobilenet.load();
+  let predictions = await pred(model, img);
+  let trials = 0;
+  while (predictions[0].probability < 0.1 && trials < 10) {
+    predictions = await pred(model, img);
+    trials += 1;
+  }
+  return predictions;
+}
+
+export {
+  mlBurst
 }
