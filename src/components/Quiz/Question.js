439733d5e5ca290224c16e4275ecc97bdf0035b7
Paola Garcia <pacri14@gmail.com>
2021-10-27 21:06:07 -0400

    Adds styles to quiz

--- src/components/Quiz/Question.js
+++ src/components/Quiz/Question.js
@@ -1,22 +1,65 @@
+import "./Quiz.css";
+
 export default function Question(props) {
   const {
     question,
-    answer, 
-    setAnswer, 
+    answer,
+    setAnswer,
     questionNumber,
     totalQuestions,
-    imageURL
+    imageURL,
   } = props;
 
   return (
-    <div>
-      <p>
-        Question: {questionNumber} / {totalQuestions}
-      </p>
-      <img src={imageURL}/>
-      <p>{question}</p>
-      <p>Your answer: </p>
-      <input value={answer} onChange={(e) => setAnswer(e.target.value)}></input>
-    </div>
+    <section className="question-card">
+      <div className="question-info">
+        <div className="info">
+          <div className="tag pink">
+            {questionNumber} / {totalQuestions}
+          </div>
+          <p>Question</p>
+        </div>
+        <div className="info">
+          <div className="tag orange">{questionNumber}</div><p>Score</p>
+        </div>
+      </div>
+      <div className="question">
+        <img src={imageURL} alt="questionimage" />
+
+        <div class="card shadow mb-3">
+          <div class="card-header py-3">
+            <p class="text-primary m-0 fw-bold">{question}</p>
+          </div>
+
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <div class="mb-3">
+                  <label class="form-label" for="username">
+                    <strong>What's the translation?</strong>
+                  </label>
+                  <input
+                    class="form-control"
+                    type="text"
+                    value={answer}
+                    onChange={(e) => setAnswer(e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    // <div>
+    //   <p>
+    //     Question: {questionNumber} / {totalQuestions}
+    //   </p>
+    //   <img src={imageURL}/>
+    //   <p>{question}</p>
+    //   <p>Your answer: </p>
+    //   <input value={answer} onChange={(e) => setAnswer(e.target.value)}></input>
+    // </div>
   );
 }
--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { getUserDictionary, auth } from "../../utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Question from "./Question";
-import Results from "./Results"; 
+import Results from "./Results";
+import "./Quiz.css";
 
 const TOTAL_QUESTIONS = 3;
 
@@ -15,10 +16,9 @@ export default function Quiz() {
   const [quizOver, setQuizOver] = useState(true);
   const [score, setScore] = useState(0);
   const [user, loading, error] = useAuthState(auth);
-   
 
   //Resets quiz and retrieves user dictionary items for the questions
-  const getThings = async () => { 
+  const getThings = async () => {
     setOnLoading(true);
     setQuizOver(false);
     //fetching user dictionary items
@@ -36,15 +36,16 @@ export default function Quiz() {
     Verifies that the current answer matches the translation for the word shown to the user
     Returns null if no answer has been provided
   */
-  const checkAnswer = () => { 
-    if(!answer){
+  const checkAnswer = () => {
+    if (!answer) {
       alert("Please add an answer");
       return;
     }
 
-    if (!quizOver) {  
+    if (!quizOver) {
       //Reviews if the answer is correct
-      const correct = questions[number].translatedWord.toLowerCase() === answer.toLowerCase();
+      const correct =
+        questions[number].translatedWord.toLowerCase() === answer.toLowerCase();
       if (correct) {
         setScore((prev) => prev + 1);
       }
@@ -54,7 +55,7 @@ export default function Quiz() {
         answer,
         correct,
         correctAnswer: questions[number].translatedWord,
-      }; 
+      };
       setUserAnswers((prev) => [...prev, answerObject]);
     }
 
@@ -62,53 +63,64 @@ export default function Quiz() {
   };
 
   //Verifies provided answer and moves to the next question
-  const nextQuestion = () => { 
-    if(checkAnswer()){
-      const nextQuestion = number + 1; 
-      setAnswer("")
-      setNumber(nextQuestion); 
-    } 
+  const nextQuestion = () => {
+    if (checkAnswer()) {
+      const nextQuestion = number + 1;
+      setAnswer("");
+      setNumber(nextQuestion);
+    }
   };
 
   //Verifies provided answer and finishes the quiz
   const reviewResults = () => {
-    if(checkAnswer()){ 
-      setAnswer("")
+    if (checkAnswer()) {
+      setAnswer("");
       setQuizOver(true);
     }
   };
 
   return (
-    <div>
-      <h1> Quiz!</h1>
-      {(quizOver || userAnswers.length === TOTAL_QUESTIONS) && (
-        <button onClick={() => getThings()}>START QUIZ</button>
-      )}
+    <div className="container-fluid">
+      
+        <h3 className="text-dark mb-0">Quiz!</h3>
+        {(quizOver || userAnswers.length === TOTAL_QUESTIONS) && (
+          <button onClick={() => getThings()}>START QUIZ</button>
+        )}
 
-      {quizOver && <p>Score: {score} </p>}
+        {quizOver && <p>Score: {score} </p>}
 
-      {onLoading && <p>Loading questions ... </p>}
+        {onLoading && <p>Loading questions ... </p>}
 
-      {!quizOver && !onLoading && (
-        <Question
-          questionNumber={number + 1}
-          totalQuestions={TOTAL_QUESTIONS}
-          question={questions[number].sourceWord}
-          imageURL={questions[number].downloadURL} 
-          callback={checkAnswer}
-          answer={answer}
-          setAnswer={setAnswer}
-        />
-      )}   
-      {!quizOver && !onLoading && number < TOTAL_QUESTIONS - 1 && (
-        <button onClick={nextQuestion}>Next</button>
-      )}
-      {!quizOver && !onLoading && number === TOTAL_QUESTIONS - 1 && (
-        <button onClick={reviewResults}>Review Results</button>
-      )}
+        {!quizOver && !onLoading && (
+        <div className="question-container">
+          <Question
+            questionNumber={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].sourceWord}
+            imageURL={questions[number].downloadURL}
+            callback={checkAnswer}
+            answer={answer}
+            setAnswer={setAnswer}
+          />
+          { number < TOTAL_QUESTIONS - 1 && (
+            <button className="control" onClick={nextQuestion}>Next</button>
+          )}
+          {number === TOTAL_QUESTIONS - 1 && (
+            <button className="control" onClick={reviewResults}>Review Results</button>
+          )}
+        </div>
+        )}
+        {/* {!quizOver && !onLoading && number < TOTAL_QUESTIONS - 1 && (
+          <button onClick={nextQuestion}>Next</button>
+        )}  
+        {!quizOver && !onLoading && number === TOTAL_QUESTIONS - 1 && (
+          <button onClick={reviewResults}>Review Results</button>
+        )}*/}
 
-      {quizOver && userAnswers.length > 0 && <Results userAnswers={userAnswers} questions={questions} />}
-       
+        {quizOver && userAnswers.length > 0 && (
+          <Results userAnswers={userAnswers} questions={questions} />
+        )}
+     
     </div>
   );
 }
